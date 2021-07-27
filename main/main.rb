7edc702fc8a78773127b94b08bfccdedc6596f87
charlesagyemang <micnkru@gmail.com>
2021-07-27 08:42:25 +0000

    Ruby methof lookup tests

--- main/main.rb
+++ main/main.rb
@@ -1,7 +1,60 @@
 
+module Anthony
+    def name
+        "Anthony"  
+    end
+end
 
-class Main
+module Martha
+    def name
+        "Martha"
+    end
+end
 
+module Ishmael
+    def name
+        "Ishmael"
+    end
+end
+
+module Emeka
+    def name
+        "Emeka"
+    end
 end
 
 
+class Main
+
+    include Anthony
+    extend  Martha
+    prepend Emeka
+    extend  Ishmael
+
+    def name
+        "Main"
+    end
+end
+
+class MainTwo
+
+    include Anthony
+    extend  Martha
+
+    def name
+        "MainTwo"
+    end
+end
+
+
+class MainThree
+    include Anthony
+    extend  Martha
+end
+
+
+class MainFour
+
+    extend  Martha
+
+end
--- main/main_spec.rb
+++ main/main_spec.rb
@@ -3,14 +3,70 @@ require_relative 'main.rb'
 describe Main do
 
     describe '::Sample Test' do 
-
         it '1 + 1 Should return Two' do
-        
             expect(1 + 1).to eq(2) 
+        end
+    end 
+
+    describe "::ancestory test instance method" do
+
+
+        it '.returns the method of the class singleton method' do
+
+            main = Main.new
+
+            def main.name
+                "Singleton Main"
+            end
+
+            expect(main.name).to      eq "Singleton Main"
 
         end
+        
+        it '.instance should look for the prepended module if singleton class doesnt exists' do
 
-    end 
+            main = Main.new
+
+            expect(main.name).to  eq "Emeka" 
+           
+        end 
+        
+        it '.instance should look for the method in its class if there is no prepend & eigenclass methods' do
+
+            main = MainTwo.new
+
+            expect(main.name).to  eq "MainTwo" 
+           
+        end 
+
+        it '.instance should look for the method in include module if theres it is not in its class, prepend and eigneclass' do
+
+            main = MainThree.new
+
+            expect(main.name).to  eq "Anthony" 
+           
+        end 
+
+        it '.instance should throw a no method error if it checks eigenclass, prepend, self and include and its not there' do
+
+            main = MainFour.new
+
+            expect{main.name}.to raise_exception(NoMethodError)
+           
+        end
+
+
+    end
+
+    describe "::ancestory test class methods" do
+        it 'return the value of the extended class' do
+
+            expect(Main.name).to  eq "Ishmael"
+            p Main.ancestors
+            
+        end
+    end
+    
 
 end 
 
